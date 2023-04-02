ad354dbaf56b2331a5ce350862794df5836a94be
Nghĩa Trần Lê Đại <20520252@ms.uit.edu.vn>
2023-04-02 22:33:10 +0700

    a

--- DroidFusion.py
+++ DroidFusion.py
@@ -1,44 +1,29 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.decomposition import IncrementalPCA, PCA
+from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
+from sklearn.impute import SimpleImputer
 import joblib
-from sklearn.preprocessing import StandardScaler
-import numpy as np
 
 # Load dataset
-dataset_file = 'D:\\uit\\bao mat web\\project mldroid\\AndroidPermission.csv'
-data = pd.read_csv(dataset_file)
+dataset_file = 'D:\\uit\\bao mat web\\project mldroid\\DATASET_S_with_Class.xlsx'
+data = pd.read_excel(dataset_file)
 
-# Perform one-hot encoding on categorical columns
-data = pd.get_dummies(data, columns=['App'])
+# Perform 5 one-hot encoding on the 5 columns
 data = pd.get_dummies(data, columns=['Package'])
 data = pd.get_dummies(data, columns=['Category'])
-data = pd.get_dummies(data, columns=['Description'])
-data = pd.get_dummies(data, columns=['Related apps'])
+
+
+# Replace missing values with the mean
+imputer = SimpleImputer(strategy='mean')
+X = data.drop('Class', axis=1)
+X = imputer.fit_transform(X)
 
 # Perform feature selection using PCA
-X = data.drop('Class', axis=1)
 y = data['Class']
-
-# Standardize the data
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-
-# Define the batch size and number of components
-batch_size = 10000
-n_components = 30
-
-# Create the Incremental PCA object
-pca = IncrementalPCA(n_components=n_components)
-
-# Apply Incremental PCA to the dataset in batches
-for batch in np.array_split(X, len(X) // batch_size):
-    pca.partial_fit(batch)
-
-# Transform the data
-X_new = pca.transform(X)
+pca = PCA(n_components=50)
+X_new = pca.fit_transform(X)
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.3, random_state=42)
@@ -53,5 +38,5 @@ accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
 # Save the trained model
-model_file = 'D:\\uit\\bao mat web\\project mldroid\\rf_model.joblib'
+model_file = 'D:\\uit\\bao mat web\\project mldroid\\ft_all_mcl_rf.joblib'
 joblib.dump(clf, model_file)
