8fdc15144e74511d09a37fc9f6495e582e827ddd
lova-52 <20520252@gm.uit.edu.vn>
2023-05-02 01:54:03 +0700

    Add Information-gain feature selection function

--- utils/feature_select.py
+++ utils/feature_select.py
@@ -34,6 +34,36 @@ def gain_ratio(X, y):
             gain_ratios[i] = information_gain / split_info
     return gain_ratios
 
+def information_gain(X, y):
+    n_features = X.shape[1]
+    X = X.astype('int64')
+    # Calculate information entropy of class variable
+    class_counts = np.bincount(y)
+    class_probs = class_counts / len(y)
+    entropy = -np.sum(class_probs * np.log2(class_probs))
+
+    # Calculate information gain for each feature
+    information_gains = np.zeros(n_features)
+    for i in range(n_features):
+        feature_values = X[:, i]
+        # Calculate information entropy of feature
+        value_counts = np.bincount(feature_values)
+        value_probs = value_counts / len(feature_values)
+        value_entropy = -np.sum(value_probs * np.log2(value_probs))
+        # Calculate information gain of feature
+        value_class_counts = np.apply_along_axis(lambda x: np.bincount(x, minlength=len(class_counts)), 0, [feature_values, y])
+        value_class_probs = value_class_counts / len(y)
+        value_entropy_class = -np.sum(value_class_probs * np.log2(value_class_probs), axis=1)
+        value_entropy_class_weighted = np.sum(value_probs * value_entropy_class)
+        information_gain = entropy - value_entropy_class_weighted
+        # Add information gain of feature
+        information_gains[i] = information_gain
+
+    # Sort features by information gain
+    feature_indices = np.argsort(information_gains)[::-1]
+    return feature_indices
+
+
 #Define feature selecting function
 def feature_selecting(feature_selection_name, X, y):
     if feature_selection_name == "FR1":
@@ -48,4 +78,9 @@ def feature_selecting(feature_selection_name, X, y):
         selector = VarianceThreshold(threshold=0.1)
         X_new = selector.fit_transform(X)
         return X_new
+    elif feature_selection_name == "FR4":
+        feature_indices = information_gain(X, y)
+        X_new = X[:, feature_indices[:20]]
+        return X_new
+
 
