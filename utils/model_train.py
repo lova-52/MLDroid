9c4ccebf45cf87b4d92c9741dde4fc103772cf93
lova-52 <20520252@gm.uit.edu.vn>
2023-05-03 03:09:24 +0700

    Fix the error of fixed result of the first dataset file

--- utils/model_train.py
+++ utils/model_train.py
@@ -31,97 +31,111 @@ from utils.dataset_loader import *
 #Ignore warnings
 warnings.filterwarnings("ignore")
 
+def choose_classifier(classifier_name):
+    if classifier_name == "SVM":
+        # Train a SVM classifier using 20-fold cross-validation
+        clf = SVC(kernel='poly', C=69, random_state=None, max_iter=1000)
+        pass
+    elif classifier_name == "NB":
+        # Train a Naive Bayes classifier using 20-fold cross-validation
+        clf = GaussianNB(var_smoothing=random.uniform(1e-9, 1e-6))
+        pass
+    elif classifier_name == "RF":
+        # Train a Random Forest classifier using 20-fold cross-validation
+        clf = RandomForestClassifier(n_estimators=100, random_state=None)
+        pass
+    elif classifier_name == "MLP":
+        # Train a MLP classifier using 20-fold cross-validation
+        clf = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=1000, random_state=None)
+        pass
+    elif classifier_name == "LR":
+        # Train a Logistic Regression classifier using 20-fold cross-validation
+        clf = clf = LogisticRegression(random_state=None)
+        pass
+    elif classifier_name == "BN":
+        # Train a Bayesian Network classifier using 20-fold cross-validation
+        clf = BayesianClassifer()
+        pass
+    elif classifier_name == "AB":
+        # Train an AdaBoost classifier using 20-fold cross-validation
+        clf = AdaBoostClassifier(n_estimators=100, random_state=None)
+        pass
+    elif classifier_name == "DT":
+        # Train a Decision Tree classifier using 20-fold cross-validation
+        clf = DecisionTreeClassifier(random_state=None)
+        pass
+    elif classifier_name == "KNN":
+        # Train a k-Nearest Neighbors classifier using 20-fold cross-validation
+        clf = KNeighborsClassifier(weights=random.choice(['uniform', 'distance']))
+        pass
+    elif classifier_name == "DNN":
+        # Train a Deep Neural Network classifier using 20-fold cross-validation
+        clf = MLPClassifier(hidden_layer_sizes=(200, 150, 100, 50), max_iter=1000)
+        pass
+    else:
+        print("Invalid classifier name.")
+    return clf
+
 def model_training(classifier_name, feature_selection_name):
 
     # List of dataset filenames
     dataset_files = load_dataset()
 
+    #List to hold confusion matricesm, accuracies, f-measure and results
     confusion_matrices = []
     accuracies = []
     f_measures = []
     results = []
+    
+    #Count the loaded datasets
+    count = 0;
 
     print("Training ", classifier_name, " classifier...")
+  
+    while True:
+        for dataset_file in dataset_files:
+            # Load dataset
+            data = pd.read_excel(f'D:\\uit\\BaoMatWeb\\MLDroid\\DATASET\\{dataset_file}')
 
-    for dataset_file in dataset_files:
-        #Print the current dataset file
-        print({dataset_file})
+            #Process data
+            y = data['Class']
+            X_new = data_processing(data)
 
-        # Load dataset
-        data = pd.read_excel(f'D:\\uit\\BaoMatWeb\\MLDroid\\DATASET\\{dataset_file}')
+            #Perform feature selection
+            X_new = feature_selecting(feature_selection_name, X_new, y)
 
-        #Process data
-        y = data['Class']
-        X_new = data_processing(data)
+            #Set the classifier
+            clf = choose_classifier(classifier_name)
+            
+            #Cross validation with k = 20
+            sk_folds = StratifiedKFold(n_splits=20, shuffle=True, random_state=None)
 
-        #Perform feature selection
-        X_new = feature_selecting(feature_selection_name, X_new, y)
+            #Train the model
+            for train_index, test_index in sk_folds.split(X_new, y):
+                X_train, X_test = X_new[train_index], X_new[test_index]
+                y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-        if classifier_name == "SVM":
-            # Train a SVM classifier using 20-fold cross-validation
-            clf = SVC(kernel='poly', C=69, random_state=None, max_iter=1000)
-            pass
-        elif classifier_name == "NB":
-            # Train a Naive Bayes classifier using 20-fold cross-validation
-            clf = GaussianNB(var_smoothing=random.uniform(1e-9, 1e-6))
-            pass
-        elif classifier_name == "RF":
-            # Train a Random Forest classifier using 20-fold cross-validation
-            clf = RandomForestClassifier(n_estimators=100, random_state=None)
-            pass
-        elif classifier_name == "MLP":
-            # Train a MLP classifier using 20-fold cross-validation
-            clf = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=1000, random_state=None)
-            pass
-        elif classifier_name == "LR":
-            # Train a Logistic Regression classifier using 20-fold cross-validation
-            clf = clf = LogisticRegression(random_state=None)
-            pass
-        elif classifier_name == "BN":
-            # Train a Bayesian Network classifier using 20-fold cross-validation
-            clf = BayesianClassifer()
-            pass
-        elif classifier_name == "AB":
-            # Train an AdaBoost classifier using 20-fold cross-validation
-            clf = AdaBoostClassifier(n_estimators=100, random_state=None)
-            pass
-        elif classifier_name == "DT":
-            # Train a Decision Tree classifier using 20-fold cross-validation
-            clf = DecisionTreeClassifier(random_state=None)
-            pass
-        elif classifier_name == "KNN":
-            # Train a k-Nearest Neighbors classifier using 20-fold cross-validation
-            clf = KNeighborsClassifier(weights=random.choice(['uniform', 'distance']))
-            pass
-        elif classifier_name == "DNN":
-            # Train a Deep Neural Network classifier using 20-fold cross-validation
-            clf = MLPClassifier(hidden_layer_sizes=(200, 150, 100, 50), max_iter=1000)
-            pass
-        else:
-            print("Invalid classifier name.")
-      
-        sk_folds = StratifiedKFold(n_splits=20, shuffle=True, random_state=None)
+                clf.fit(X_train, y_train)
+                y_pred = clf.predict(X_test)
 
-        for train_index, test_index in sk_folds.split(X_new, y):
-            X_train, X_test = X_new[train_index], X_new[test_index]
-            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+                confusion_matrices.append(confusion_matrix(y_test, y_pred))
 
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
+                accuracy = clf.score(X_test, y_test)
+                accuracies.append(accuracy)
 
-            confusion_matrices.append(confusion_matrix(y_test, y_pred))
+                f_measure = f1_score(y_test, y_pred, average='weighted')
+                f_measures.append(f_measure)
 
-            accuracy = clf.score(X_test, y_test)
-            accuracies.append(accuracy)
+            accuracy_mean = np.mean(accuracies)
+            f_measure_mean = np.mean(f_measures)
 
-            f_measure = f1_score(y_test, y_pred, average='weighted')
-            f_measures.append(f_measure)
-
-        accuracy_mean = np.mean(accuracies)
-        f_measure_mean = np.mean(f_measures)
-
-
-        results.append((dataset_file, accuracy_mean, f_measure_mean))
+            #Save the results to print them later
+            results.append((dataset_file, accuracy_mean, f_measure_mean))
+            count += 1
+            if count == 31:
+                break  
+        if count == 31:
+            break   
 
     #Sort results
     results_sorted = sorted(results, key=lambda x: int(re.findall(r'\d+', x[0])[0]))
